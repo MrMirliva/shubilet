e68709a789fc663df0e123c390bbd06ba8a99c27
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:24:40 +0300

    feat: implement CustomerService interface for customer management

---
+++ backend/member-service/src/main/java/com/shubilet/member_service/services/CustomerService.java
@@ -0,0 +1,8 @@
+package com.shubilet.member_service.services;
+
+import com.shubilet.member_service.models.Customer;
+
+public interface CustomerService {
+    ///TODO: Test method, remove after tests
+    void saveCustomer(Customer customer);
+}
