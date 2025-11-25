b72d874c95c9d1281d87114861de3b8e11d903a7
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:27:05 +0300

    feat: create CustomerSessionRepository for managing customer sessions

---
+++ backend/security-service/src/main/java/com/shubilet/security_service/repositories/CustomerSessionRepository.java
@@ -0,0 +1,26 @@
+package com.shubilet.security_service.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shubilet.security_service.models.CustomerSession;
+
+
+///TOTO: Tüm queryler yazılacak.
+@Repository
+public interface CustomerSessionRepository extends JpaRepository<CustomerSession, Integer> {
+   
+    ///TODO: Query düzenlenip aktifleştirilecek.
+    //@Query("SELECT CASE WHEN COUNT(a) > 0 THEN true ELSE false END FROM CustomerSession a WHERE a.email = ?1 AND a.password = ?2")
+    //boolean isEmailAndPasswordValid(String email, String password);
+
+    // boolean createSession(CustomerSession customerSession);
+
+    // boolean deleteSession(int id);
+
+    // boolean isIdExist(int id);
+
+    // boolean isCodeExist(String code);
+
+    // boolean sessionExist(int id, String code);
+}
