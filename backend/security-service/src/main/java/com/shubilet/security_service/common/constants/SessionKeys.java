a2a7b82c799e45f1cbc9627b8071fa8909699bad
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:25:52 +0300

    feat: add SessionKeys class for common session key constants

---
+++ backend/security-service/src/main/java/com/shubilet/security_service/common/constants/SessionKeys.java
@@ -0,0 +1,12 @@
+package com.shubilet.security_service.common.constants;
+
+public final class SessionKeys {
+    private SessionKeys() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    public static final String USER_ID = "userId";
+    public static final String USER_TYPE = "userType";
+    public static final String AUTH_CODE = "authCode";
+}
+
