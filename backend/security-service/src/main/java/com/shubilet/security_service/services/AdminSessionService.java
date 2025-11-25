5b4c7e2a3a33b7fd257c7fce87c306683728a067
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:27:09 +0300

    feat: create AdminSessionService interface for admin session management

---
+++ backend/security-service/src/main/java/com/shubilet/security_service/services/AdminSessionService.java
@@ -0,0 +1,15 @@
+package com.shubilet.security_service.services;
+
+import org.springframework.http.ResponseEntity;
+
+import com.shubilet.security_service.dataTransferObjects.requests.SessionInfoDTO;
+
+public interface AdminSessionService {
+    public ResponseEntity<SessionInfoDTO> login(String email, String password);
+
+    public ResponseEntity<Boolean> logout(int id);
+
+    public ResponseEntity<Boolean> check(int id, String token);
+
+    public boolean hasEmail(String email);
+}
