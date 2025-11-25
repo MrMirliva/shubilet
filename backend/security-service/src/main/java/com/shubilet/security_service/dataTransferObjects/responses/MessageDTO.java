ed39b4f936f6a20e53b99c7c9f41715d98bb901e
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:26:34 +0300

    feat: add MessageDTO for response handling

---
+++ backend/security-service/src/main/java/com/shubilet/security_service/dataTransferObjects/responses/MessageDTO.java
@@ -0,0 +1,17 @@
+package com.shubilet.security_service.dataTransferObjects.responses;
+
+public class MessageDTO {
+    private String message;
+
+    public MessageDTO(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
