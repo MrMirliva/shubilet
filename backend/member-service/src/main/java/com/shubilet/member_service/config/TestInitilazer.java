7a1aacba549c1f5b755a5306190febb00a8ee3e7
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:23:41 +0300

    feat: implement TestInitilazer for application initialization

---
+++ backend/member-service/src/main/java/com/shubilet/member_service/config/TestInitilazer.java
@@ -0,0 +1,22 @@
+package com.shubilet.member_service.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.shubilet.member_service.services.CustomerService;
+
+@Component
+public class TestInitilazer implements CommandLineRunner{
+
+    private final CustomerService customerService;
+
+    public TestInitilazer(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        System.out.println("Initialization completed.");
+    }
+}
