3748f074e7ba9af788c23b6a4e81b2e591b03f73
Abdullah Gündüz (Mirliva) <abdullah.gndz00@gmail.com>
2025-11-26 01:24:45 +0300

    feat: implement CustomerServiceImpl for customer management

---
+++ backend/member-service/src/main/java/com/shubilet/member_service/services/Impl/CustomerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.shubilet.member_service.services.Impl;
+
+import org.springframework.stereotype.Service;
+
+import com.shubilet.member_service.models.Customer;
+import com.shubilet.member_service.repositories.CustomerRepository;
+import com.shubilet.member_service.services.CustomerService;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    ///TODO: Just a test method, remove later.
+    public void saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+}
