46e065023c2c61751777b1ef68f93f96cbc0b828
Murat Furkan Şen <muratfurkan.sen79@gmail.com>
2025-12-14 15:34:37 +0300

    add: Introduce `AuthController` interface for registration and authentication operations
    Created a new `AuthController` interface in `api-gateway` to standardize and centralize registration and authentication endpoints. This addition includes methods for registering customers, companies, and admins, as well as login and logout functionality.
    
    Key updates:
    - Added `registerCustomer`, `registerCompany`, and `registerAdmin` methods to handle role-specific registrations.
    - Introduced `login` and `logout` endpoints for managing user sessions.
    - Leveraged DTOs such as `CustomerRegistrationDTO`, `CompanyRegistrationDTO`, `AdminRegistrationDTO`, and `MemberCredentialsDTO` for request payloads.
    
    This interface lays the foundation for consistent authentication operations and improves modularity within the authentication flow.

---
+++ backend/api-gateway/src/main/java/com/shubilet/api_gateway/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.shubilet.api_gateway.controllers;
+
+import com.shubilet.api_gateway.dataTransferObjects.MessageDTO;
+import com.shubilet.api_gateway.dataTransferObjects.external.requests.auth.AdminRegistrationDTO;
+import com.shubilet.api_gateway.dataTransferObjects.external.requests.auth.CompanyRegistrationDTO;
+import com.shubilet.api_gateway.dataTransferObjects.external.requests.auth.CustomerRegistrationDTO;
+import com.shubilet.api_gateway.dataTransferObjects.external.requests.auth.MemberCredentialsDTO;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public interface AuthController {
+    @PostMapping("/register/customer")
+    ResponseEntity<MessageDTO> registerCustomer(HttpSession httpSession, @RequestBody CustomerRegistrationDTO customerRegistrationDTO);
+
+    @PostMapping("/register/company")
+    ResponseEntity<MessageDTO> registerCompany(HttpSession httpSession, @RequestBody CompanyRegistrationDTO companyRegistrationDTO);
+
+    @PostMapping("/register/admin")
+    ResponseEntity<MessageDTO> registerAdmin(HttpSession httpSession, @RequestBody AdminRegistrationDTO adminRegistrationDTO);
+
+    @PostMapping("/login")
+    ResponseEntity<MessageDTO> login(HttpSession session, @RequestBody MemberCredentialsDTO memberCredentialsDTO);
+
+    @PostMapping("/logout")
+    public ResponseEntity<MessageDTO> logout(HttpSession httpSession);
+}
